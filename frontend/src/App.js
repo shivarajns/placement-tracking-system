164bf8d58ce11add599d084fdcffc638380b05cc
Shivaraju NS <shivarajsmurthy@gmail.com>
2026-02-01 08:55:50 +0530

    chor: Added Fallback home page Route to resolve initial enpty page rendering

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="*" element={<Home/>}/>
         <Route path="/login" element={<Login/>}/>
       </Routes>
       <Footer/>
