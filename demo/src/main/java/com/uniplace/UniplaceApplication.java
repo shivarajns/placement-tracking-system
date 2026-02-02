ea0ae0d2de985eec7159e467d5e8256a7fa74a90
Shivaraju NS <shivarajsmurthy@gmail.com>
2026-02-02 20:18:08 +0530

    feat: initialize Spring Boot project with MySQL integration

---
+++ demo/src/main/java/com/uniplace/UniplaceApplication.java
@@ -0,0 +1,13 @@
+package com.uniplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniplaceApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/uniplace/UniplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
