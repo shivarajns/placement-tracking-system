07030c859533e3de69e36ab91fcd14859baa14c3
Shivaraju NS <shivarajsmurthy@gmail.com>
2026-02-13 20:42:00 +0530

    feat: Updated SecurityConfig file, inject AuthenticationPower Bean with User Details and Authentication Manager with Authentication Provider, Update Security Filter Chain.

---
+++ backend/src/main/java/com/Uniplace/backend/Config/JwtUtil.java
@@ -0,0 +1,8 @@
+package com.Uniplace.backend.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+}
--- backend/src/main/java/com/Uniplace/backend/Config/SecurityConfig.java
+++ backend/src/main/java/com/Uniplace/backend/Config/SecurityConfig.java
@@ -1,7 +1,11 @@
 package com.Uniplace.backend.Config;
 
+import com.Uniplace.backend.Service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,12 +14,35 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final CustomUserDetailsService customUserDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    public SecurityConfig(CustomUserDetailsService customUserDetailsService, PasswordEncoder passwordEncoder){
+        this.customUserDetailsService = customUserDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(customUserDetailsService);
+        provider.setPasswordEncoder(passwordEncoder.PasswordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager (AuthenticationProvider provider){
+        return new org.springframework.security.authentication.ProviderManager(provider);
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
         http
                 .csrf(csrf->csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll());
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                );
 
         return http.build();
     }
