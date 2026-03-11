f693468fd0c5aef762b205781df361d910311c36
Shivaraju NS <shivarajsmurthy@gmail.com>
2026-03-11 16:22:32 +0530

    feat(Recruiter): Recruiter model is created and connected with a recruiter database.

--- backend/src/main/java/com/Uniplace/backend/Model/Recruiter.java
+++ backend/src/main/java/com/Uniplace/backend/Model/Recruiter.java
@@ -1,19 +1,35 @@
-//package com.Uniplace.backend.Model;
-//
-//
-//import jakarta.persistence.*;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Entity
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Table(name = "recruiter")
-//public class Recruiter {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private string
-//}
+package com.Uniplace.backend.Model;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "recruiter")
+public class Recruiter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "recruiter_id")
+    private int recruiterId;
+
+    @Column(name = "recruiter_name")
+    private String recruiterName;
+
+    private String email;
+
+    private String phone;
+
+    @Column(name = "desigination")
+    private String designation;
+
+    @Column(name = "recruiter_bio")
+    private String recruiterBio;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company company;
+
+    @OneToMany(mappedBy = "recruiter")
+    private List<Job> jobs;
+
+}
